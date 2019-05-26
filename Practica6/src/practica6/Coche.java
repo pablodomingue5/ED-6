1b9fb1945ee5659fc90c82eceeb3666a66c91ed3
PABLO <PABLO@DESKTOP-MHJTQ14>
2019-05-26 20:13:03 +0200

    JavaDoc creado y comentado

--- Practica6/src/practica6/Coche.java
+++ Practica6/src/practica6/Coche.java
@@ -1,5 +1,9 @@
 package practica6;
-
+/**
+ * 
+ * @author PABLO
+ * @version 1.2
+ */
 public class Coche {
 	String matricula;
 	String atrib;
@@ -12,14 +16,17 @@ public class Coche {
 		modelo = "";
 		fabricante = "";
 	}
-
+	
 	public Coche(String m, String c, String mo, String f) {
 		matricula = m;
 		atrib = c;
 		modelo = mo;
 		fabricante = f;
 	}
-	
+	/**
+	 * 
+	 * @return Devuelve El número de la matrícula del coche seleccionado y una pequeña descripción del mismo
+	 */
 	public String metodo1() { //Aplicada la refactorizacion
 		String resultado = "El coche con matricula " + matricula + " ";
 		switch(atrib) {
@@ -39,16 +46,33 @@ public class Coche {
 		return resultado;
 	}
 	
+	/**
+	 * 
+	 * @param modelo3 Variable recogida de el modelo seleccionado 
+	 * @param fabricante3 Variable recogida de el fabricante seleccionado
+	 * @return
+	 */
 	public String metodoC(String modelo3, String fabricante3) {
 		String resultado="es un "+fabricante3+" "+modelo3+" y no necesita combustible.";
 		return resultado;
 	}
-
+	/**
+	 * 
+	 * @param modelo2 Variable recogida de el modelo seleccionado
+	 * @param fabricante2 Variable recogida de el fabricante seleccionado
+	 * @return
+	 */
 	public String metodoB(String modelo2, String fabricante2) {
 		String resultado="es un "+fabricante2+" "+modelo2+" y gasta 1,052 euros por litro.";
 		return resultado;
 		
 	}
+	/**
+	 * 
+	 * @param modelo Variable recogida de el modelo seleccionado
+	 * @param fabricante Variable recogida de el fabricante seleccionado
+	 * @return
+	 */
 
 	public String metodoA(String modelo, String fabricante) {
 		String resultado="es un "+fabricante+" "+modelo+" y gasta 1,337 euros por litro.";
