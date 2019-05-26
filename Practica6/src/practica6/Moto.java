0a79db193cb6ee0ac62b1b2997ac19dd4088a724
PABLO <PABLO@DESKTOP-MHJTQ14>
2019-05-26 20:59:25 +0200

    Versión final

--- Practica6/src/practica6/Moto.java
+++ Practica6/src/practica6/Moto.java
@@ -1,5 +1,9 @@
 package practica6;
-
+/**
+ * 
+ * @author PABLO
+ * @version 1.0
+ */
 public class Moto {
 	String matricula;
 	int cilindrada;
@@ -9,23 +13,44 @@ public class Moto {
 		cilindrada = 0;
 	}
 
+	/**
+	 * 
+	 * @param m matricula de la moto
+	 * @param c cilindrada de la moto
+	 */
 	public Moto(String m, int c) {
 		matricula = m;
 		cilindrada = c;
 	}
+	
+	/**
+	 * 
+	 * @return Devuelve la matricula de la moto
+	 */
 
 	public String getMatricula() {
 		return matricula;
 	}
 
+	/**
+	 * 
+	 * @return Devuelve la cilindrada de la moto
+	 */
 	public int getCilindrada() {
 		return cilindrada;
 	}
-
+	/**
+	 * 
+	 * @param m atributo matricula
+	 */
 	public void setMatricula(String m) {
 		matricula = m;
 	}
 
+	/**
+	 * 
+	 * @param c atributo cilindrada
+	 */
 	public void setCilindrada(int c) {
 		cilindrada = c;
 	}
